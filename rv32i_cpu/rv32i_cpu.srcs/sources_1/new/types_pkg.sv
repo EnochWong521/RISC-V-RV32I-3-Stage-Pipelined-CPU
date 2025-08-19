ff793968032a1fd4f1d1893e1e100f3771ab7912
Enoch Wong <enoch3d14@gmail.com>
2025-08-18 19:54:59 -0700

    Package file

---
+++ rv32i_cpu/rv32i_cpu.srcs/sources_1/new/types_pkg.sv
@@ -0,0 +1,23 @@
+// rtl/types_pkg.sv
+package types_pkg;
+    // ALU operations
+    typedef enum logic [3:0] {
+        ALU_SUB,
+        ALU_AND,
+        ALU_OR,
+        ALU_XOR
+    } alu_op_e;
+
+    // control signals derived from instruction decode
+    typedef struct packed {
+        logic use_imm;
+        logic alu_op;
+        logic reg_write;
+    } ctrl_t;
+
+    // 12 bit sign extension for I-type immediate functions
+    function automatic logic [31:0] sign_ext12(input logic [11:0] imm12);
+        // implement sign extension. replicate bit 11 into upper 20 bits
+        sign_ext12 = {{20{imm12[11]}}, imm12};
+    endfunction
+endpackage
