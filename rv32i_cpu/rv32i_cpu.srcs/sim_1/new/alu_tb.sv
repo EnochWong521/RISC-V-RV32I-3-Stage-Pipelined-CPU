09ebc85226a86eb17eac262dadc7c17141f26b76
Enoch Wong <enoch3d14@gmail.com>
2025-08-23 14:38:06 -0700

    Immediate generator testbench

---
+++ rv32i_cpu/rv32i_cpu.srcs/sim_1/new/alu_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/23/2025 02:35:12 PM
+// Design Name: 
+// Module Name: alu_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module alu_tb(
+
+    );
+endmodule
--- rv32i_cpu/rv32i_cpu.srcs/sources_1/new/alu.sv
+++ rv32i_cpu/rv32i_cpu.srcs/sources_1/new/alu.sv
@@ -1,3 +1,5 @@
+`timescale 1ns / 1ps
+
 import types_pkg::*;
 
 // alu ports
